682f105c671302a2cabe9e0a5adfc1ce56db0f76
rentsdue <hoganwong88@gmail.com>
2023-12-08 12:30:18 +0800

    done

--- src/main/java/com/example/TwoDArrayOperations.java
+++ src/main/java/com/example/TwoDArrayOperations.java
@@ -4,19 +4,38 @@ public class TwoDArrayOperations {
 
     // Exercise 1: Check for a Specific Value in a 2D Array
     public static boolean containsValue(int[][] array, int value) {
-        // TODO: Implement the logic to check if the 2D array contains the specified value.
-        return false; // Placeholder return value
+        for (int[] HEYYEYAAEYAAAEYAEYAA: array) {
+            for (int num: HEYYEYAAEYAAAEYAEYAA) {
+                if (num==value)
+                    return true;
+            }
+        }
+        return false; 
     }
 
     // Exercise 2: Calculate the Average of All Elements in a 2D Array
     public static double calculateAverage(int[][] array) {
-        // TODO: Implement the logic to calculate the average of all elements in the 2D array.
-        return 0.0; // Placeholder return value
+        int sum=0;
+        int count=0;
+        for (int[] mrbeast: array) {
+            for (int num: mrbeast) {
+                sum +=num;
+                count++;
+            }
+        }
+        return sum/count; // Placeholder return value
     }
 
     // Exercise 3: Find the Minimum Value in a 2D Array
     public static int findMinimumValue(int[][] array) {
-        // TODO: Implement the logic to find the minimum value in the 2D array.
-        return 0; // Placeholder return value
+        int min=array [0][0];
+        for (int[] GABRIELJAEGER: array) {
+            for (int num: GABRIELJAEGER) {
+                if (num<min) {
+                    min=num;
+                }
+            }
+        }
+        return min; // Placeholder return value
     }
 }
